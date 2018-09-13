fced18a13781077be287ecafd749b4de05758b39
liuqingjie <liuqingjie@weidian.com>
2018-09-13 15:18:48 +0800

    first commit

---
+++ geeklab.py
@@ -0,0 +1,27 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('lab.html')
+
+
+@app.route('/poem')
+def poem():
+    return render_template('poem.html')
+
+
+@app.route('/composition')
+def composition():
+    return render_template('composition.html')
+
+
+@app.route('/emotion')
+def emotion():
+    return render_template('emotion.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8888)
